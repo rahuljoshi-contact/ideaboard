d41ede3dd168eaad4bcbc0ff4ef897af7e723896
rahuljoshi-contact <rahuljoshi.contact@gmail.com>
2018-02-15 10:44:51 -0500

    ideaboard-api added to git

---
+++ ideaboard-api/app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,21 @@
+module Api::V1
+  class IdeasController < ApplicationController
+    def index
+      @ideas = Idea.order('created_at desc');
+      render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
+
+
+  end
+end
---
+++ ideaboard-api/ideaboard/src/components/IdeasContainer.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import Idea from './idea';
+import update from 'immutability-helper';
+
+class IdeasContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/ideas.json')
+    .then(response => {
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '<enter your title>',
+          body: '<enter your body>'
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+      const ideas = update(this.state.ideas, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({ideas: ideas})
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button onClick={this.addNewIdea} className="newIdeaButton">
+            New Idea
+          </button>
+        </div>
+      {this.state.ideas.map((idea) => {
+        return (<Idea idea={idea} key={idea.id} />)
+      })}
+      </div>
+
+    )
+  }
+}
+
+export default IdeasContainer
